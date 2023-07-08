540ce77435eb9af08e7f8f7ef1f2cfce34872075
cranatic101 <suhaasvellanki@hotmail.com>
2023-07-08 11:37:32 -0700

    updated HomeC.csv, testing out the data in main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def index():
-    return render_template("index.html")
+    r='test'
+    df = pd.read_csv("HomeC.csv", low_memory=False)
+    print(df.head(3))
+    return render_template("index.html",r=df.head(3))
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
