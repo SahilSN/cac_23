a2cd2d977758e61a210186fc1e52457cfbb87e94
cranatic101 <suhaasvellanki@hotmail.com>
2023-07-18 14:01:59 -0700

    trying to delete rows on csv file

--- delete_rows.py
+++ delete_rows.py
@@ -3,6 +3,10 @@ df=pd.read_csv('csv_data/incremented_use.csv')
 
 for i, row in df.iterrows():
     # Check if the row should be deleted
-    if str(row['time'])[14:15] not in ['00','15','30','45']:
+    if str(row['time'])[14:16] not in ['00','15','30','45']:
+
         # Delete the row
         df.drop(i, inplace=True)
+
+print(df.head(3))
+df.to_csv('csv_data/incremented_use.csv',index=False)
