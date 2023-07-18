c7ef3ef558a8d614d3ffcf35caef1f150220abbd
cranatic101 <suhaasvellanki@hotmail.com>
2023-07-18 14:04:34 -0700

    comments on delete_row.py

--- delete_rows.py
+++ delete_rows.py
@@ -1,6 +1,7 @@
 import pandas as pd
-df=pd.read_csv('csv_data/incremented_use.csv')
+df=pd.read_csv('csv_data/use_HO.csv')
 
+#drops rows in a dataframe
 for i, row in df.iterrows():
     # Check if the row should be deleted
     if str(row['time'])[14:16] not in ['00','15','30','45']:
@@ -8,5 +9,6 @@ for i, row in df.iterrows():
         # Delete the row
         df.drop(i, inplace=True)
 
+#writes dataframe back to the csv file
 print(df.head(3))
 df.to_csv('csv_data/incremented_use.csv',index=False)
