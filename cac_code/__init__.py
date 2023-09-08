9a2409aad4883f827ee77a4f32efd21e09d4555d
cranatic101 <suhaasvellanki@hotmail.com>
2023-09-08 10:09:34 -0700

    test

--- cac_code/__init__.py
+++ cac_code/__init__.py
@@ -1,4 +1,4 @@
 from flask import Flask
 app = Flask(__name__)
-print('a')
+print('AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH')
 import cac_code.views
--- cac_code/views.py
+++ cac_code/views.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import sys
 from cac_code import app
-
+print('BAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHh')
 @app.route("/",methods=['GET','POST'])
-@app.route("/dashboard",methods=['GET','POST'])
 def index():
     print('indexxx')
     return render_template("index.html")
