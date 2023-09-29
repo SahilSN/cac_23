97feed0cbf7a5aac062c3fc7be30e503b29ff88a
cranatic101 <suhaasvellanki@hotmail.com>
2023-09-28 17:40:01 -0700

    added landing to views

--- cac_code/battery_var.py
+++ cac_code/battery_var.py
@@ -47,39 +47,3 @@ for datetime in datetimes_after:
 battery_df['battery'] = battery_list
 battery_df['waste'] = waste_list
 battery_df.to_csv('cac_code/csv_data/battery_data.csv',index=False)
-
-""" today='2023-07-14 22:12:00'
-
-battery=0
-waste=0
-x=0
-battery_df['waste']=''
-while x <= use_df.loc[use_df['time']==today].index.values[0]:
-    battery+=gen_df.loc[x].gen_Sol
-    battery-=use_df.loc[x].use_HO
-    if battery<=0:
-        waste+=battery
-        battery=0
-
-  
-    battery_df.loc[x,'battery']=battery
-
-    battery_df.loc[x, 'waste'] = round(waste,2)
-    x+=1
-
-
-
-while x >= use_df.loc[use_df['time']==today].index.values[0]:
-    battery+=gen_df.loc[x].gen_Sol
-    battery-=random.uniform(house.pred_cons(use_df.loc[x].time)-0.1, house.pred_cons(use_df.loc[x].time)+0.1)
-    if battery<=0:
-        waste+=battery
-        battery=0
-
-    battery_df.loc[x,'battery']=battery
-
-    battery_df.loc[x, 'waste'] = round(waste,2)
-    x+=1
-
-
-battery_df.to_csv('csv_data/battery_data.csv',index=False) """
--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='cac23',
-    packages=['cac_code'],
-    include_package_data=True,
-    install_requires=['flask'],
-)
