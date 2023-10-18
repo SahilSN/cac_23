76f987f18f805591d8b9ce2b4a6bb2536d653695
cranatic101 <suhaasvellanki@hotmail.com>
2023-10-17 17:22:46 -0700

    style expirement with dashbaord

--- cac_code/charts_class.py
+++ cac_code/charts_class.py
@@ -18,21 +18,25 @@ def generate_line(df, x_col, y_col_s, y_col_e, title):
                             color_discrete_sequence=["#7DA1FB","#7DFB89"])
     plotly_fig.update_traces(line=dict(width=2.2))
     plotly_fig.update_layout(
-        plot_bgcolor='white'
+        plot_bgcolor='rgba(36,37,45,255)',
+        paper_bgcolor='rgba(36,37,45,255)',
+        font_color="white",
+        title_font_color="white",
+        legend_title_font_color="white"
     )
     plotly_fig.update_xaxes(
         #mirror=True,
         ticks='inside',
         #showline=True,
         #linecolor='black',
-        gridcolor='lightgrey'
+        gridcolor='white'
     )  
     plotly_fig.update_yaxes(
         #mirror=True,
         ticks='inside',
         #showline=True,
         #linecolor='black',
-        gridcolor='lightgrey'
+        gridcolor='white'
     )
     #plotly_fig.show()
     
@@ -55,9 +59,13 @@ def generate_pie(df,colors = None):
             textinfo='label+percent',
             textfont_size=12,
             marker=dict(colors=colors, 
-                        line=dict(color='#FFFFFF', width=2))
+                        line=dict(color='rgba(36,37,45,255)', width=2))
     )
-    fig.update_layout(margin=dict(t=0.2, b=0.2, l=0.2, r=0.2))
+    fig.update_layout(plot_bgcolor='rgba(36,37,45,255)',paper_bgcolor='rgba(36,37,45,255)',
+                    font_color="white",
+                    title_font_color="white",
+                    legend_title_font_color="white",
+                    margin=dict(t=0.2, b=0.2, l=0.2, r=0.2))
     #fig.show()
     div = plotly.offline.plot(fig, include_plotlyjs=False, output_type='div', config={'displayModeBar': False})
     return div
@@ -81,8 +89,11 @@ def generate_heatmap(df,colors=None):
                 xaxis={'side': 'bottom'},
                 yaxis_showgrid=False,
                 yaxis_autorange='reversed',                   
-                paper_bgcolor=None,
-                plot_bgcolor='white'
+                plot_bgcolor='rgba(36,37,45,255)',
+                paper_bgcolor='rgba(36,37,45,255)',
+                font_color="white",
+                title_font_color="white",
+                legend_title_font_color="white"
     )
     
     #fig.show()  
