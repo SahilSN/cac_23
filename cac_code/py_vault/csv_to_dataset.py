bee429f40d91330830d14f71d9a6ad81943b3896
cranatic101 <suhaasvellanki@hotmail.com>
2023-10-28 16:44:21 -0700

    consumption scalars finally good enough i think

--- cac_code/py_vault/csv_to_dataset.py
+++ cac_code/py_vault/csv_to_dataset.py
@@ -63,7 +63,7 @@ df_gen=df.drop(columns=["Dishwasher", "Home office", "Fridge", "Wine cellar", "G
                         "Microwave", "Living room", "Furnace", "Kitchen","year","day","weekofyear","minute","timing"
 ,"use_HO"])
 
-df_gen['gen_Sol'] = df_gen['gen_Sol'].apply(lambda x: x*0.1)
+df_gen['gen_Sol'] = df_gen['gen_Sol'].apply(lambda x: x*0.3)
 hours=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,0]
 scalars=[0.002,0.002,0.002,0.002,0.004,0.006,0.006,0.006,0.006,0.004,0.003,0.002,0.001,0.001,0.001,0.002,0.004,0.007,0.009,0.009,0.009,0.006,0.003,0.002]
 for i in range(len(hours)):
